bf16483862882a1b97f52da920e6323b34b6f2d8
Peter <peter.schenkels@student.hu.nl>
2019-04-22 12:40:24 +0200

    new file:   1_os_level/.idea/1_os_level.iml 	new file:   1_os_level/.idea/misc.xml 	new file:   1_os_level/.idea/modules.xml 	new file:   1_os_level/.idea/vcs.xml 	new file:   1_os_level/.idea/workspace.xml 	new file:   1_os_level/tool 	modified:   1_os_level/tool.cc

--- 1_os_level/tool.cc
+++ 1_os_level/tool.cc
@@ -1,18 +1,60 @@
 #include <iostream>
 #include <string>
 
-std::string translate(std::string input, std::string variabele)
-{ std::string result = ""; // implementeer dit
-  return result; }
+std::string translate(std::string input, std::string variabele){
+    std::string result = "";// implementeer dit
+    for(unsigned int i =0; i < variabele.size(); i++){
+        if(variabele[i] == 'e'){
 
-int main(int argc, char *argv[])
-{ std::string s;
+            result += "3";
 
-  if(argc != 2)
-  { std::cerr << "Deze functie heeft exact 1 argument nodig";
-    return -1; }
 
-  while(true)
-  { std::cin >> s;
-    if(std::cin.eof()){ return 0; }
-    std::cout << translate(argv[1], s) << std::endl; } }
+        }
+        else if(variabele[i] == 'l'){
+
+            result += "1";
+
+        }
+        else if(variabele[i] == 't'){
+
+            result += "7";
+
+        }
+        else if(variabele[i] == 'o'){
+
+            result += "0";
+
+        }
+        else if(96 < int(variabele[i]) and int(variabele[i]) < 123){
+
+            result += variabele[i]-32;
+        }
+        else{
+
+            result += variabele[i];
+
+        }
+
+
+
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[]){
+    std::string s;
+
+    if(argc != 2){
+      std::cerr << "Deze functie heeft exact 1 argument nodig";
+      return -1;
+  }
+
+  while(true){
+
+      std::cin >> s;
+      if(std::cin.eof()){
+          return 0;
+      }
+
+      std::cout << translate(argv[1], s) << std::endl; } }
